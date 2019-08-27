b62e15b1c603b0755cb3789b07da616e6cf65f27
Jamey-Dogom <33010705+AppJedi@users.noreply.github.com>
2019-08-27 10:35:19 -0700

    Convert to MVC design pattern

--- toDone/Controllers/ToDoListViewController.swift
+++ toDone/Controllers/ToDoListViewController.swift
@@ -9,13 +9,16 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-
-    var itemArray = ["Clean house", "Buy Groceries", "Wash Car"]
+    
+    // an array of Item objects
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "ToDoArray") as? [String] {
+        
+        
+        if let items = defaults.array(forKey: "ToDoArray") as? [Item] {
             itemArray = items
         }
         // Do any additional setup after loading the view.
@@ -31,7 +34,13 @@ class ToDoListViewController: UITableViewController {
     // inputting data into each cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
     }
     
@@ -42,12 +51,9 @@ class ToDoListViewController: UITableViewController {
 //        print(itemArray[indexPath.row])
         
         // add a checkmark to the selected item
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        // reload the table
+        tableView.reloadData()
         
         // have the gray highlight of the selected cell go away
         tableView.deselectRow(at: indexPath, animated: true)
@@ -65,7 +71,11 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen when user clicks the action
-            self.itemArray.append(textField.text!)
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray, forKey: "ToDoArray")
             
             // reload the information
---
+++ toDone/Models/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  toDone
+//
+//  Created by Jamey Dogom on 8/27/19.
+//  Copyright © 2019 Jamey Dogom. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+
+    var title : String = ""
+    var done : Bool = false
+    
+    
+}
